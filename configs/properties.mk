0948d74ffd7b9cd171e40810fccedbf8b0077c91
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-01-23 20:53:39 -0300

    extras: Optimize package manager dexopt properties
    * first-boot is set to quicken to reduce first boot times
    * we are installing with speed-profile to have balance
      between time taken to optimise and runtime performance
    * once device is in idle+charging, bg dexopt will compile
      for maximum runtime performance
    
    [ghostrider-reborn]
    - Stripped from https://gerrit.aospa.co/c/AOSPA/android_vendor_aospa/+/20637
    
    Change-Id: I1fde631e528980200d171843f250ad126bd6386e
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- configs/properties.mk
+++ configs/properties.mk
@@ -7,6 +7,11 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     sys.vendor.shutdown.waittime=500
 
+# Dexopt
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    pm.dexopt.first-boot=quicken
+    pm.dexopt.bg-dexopt=everything
+
 # Mobile Data
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.com.android.mobiledata=false
