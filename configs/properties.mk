de234639e6106e64b78ea5fdc99329a3e7ed3186
ImSpiDy <SpiDy2713@gmail.com>
2024-02-15 22:09:09 -0300

    extras: Set dex2oat filter to quicken
    * Run DEX code verification and optimize some DEX
      instructions to get better interpreter performance.
    
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- configs/properties.mk
+++ configs/properties.mk
@@ -1,3 +1,8 @@
+# ART
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    dalvik.vm.dex2oat-filter=quicken
+    dalvik.vm.image-dex2oat-filter=quicken
+
 # Boot
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     sys.vendor.shutdown.waittime=500
